56cf808e640954c0276faa4d9b3afb51ce53a03f
Marcio <mjteixeira@gmail.com>
2020-03-09 20:10:20 -0300

    minor

--- RUN/scheduling-performance/sch78kf.sh
+++ RUN/scheduling-performance/sch78kf.sh
@@ -5,15 +5,15 @@ _COUNT=1
 _NB_SEEDS=10
 
 until [ $_COUNT -gt $_NB_SEEDS ]; do
-for sched in 1 2 3 4 5 6 7 8 
+for sched in 7 8  
 do
 for ue in 10 20 30 40	#number of users
 do
 for del in 0.04 0.1 	#target delay
 do
-for v in 0 3 30 120 150 300 350 		#users speed
+for v in  3 30 120 		#so far this is what is available
 do
-	../../LTE-Sim SingleCellWithI 19 0.5 $ue 1 1 1 0 $sched 1 $v $del 128 > TRACE/SCHED_${sched}_UE_${ue}_V_${v}_D_${del}_$_COUNT
+	../../5G-air-simulator SingleCellWithI 19 0.5 $ue 1 1 1 0 $sched 1 $v $del 128 > TRACE/SCHED_${sched}_UE_${ue}_V_${v}_D_${del}_$_COUNT
 	cd TRACE
 	gzip SCHED_${sched}_UE_${ue}_V_${v}_D_${del}_$_COUNT
 	cd ..
